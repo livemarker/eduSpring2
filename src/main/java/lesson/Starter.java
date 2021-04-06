cfbafd5398a28ee5232fb7ed3c9f7912d10190d9
Elovenko Nikita <livemarker@users.noreply.github.com>
2021-04-06 12:40:06 +0300

    added @Autowired annotation

--- src/main/java/lesson/Starter.java
+++ src/main/java/lesson/Starter.java
@@ -9,10 +9,14 @@ public class Starter {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-
-        Music music = context.getBean("classicalMusic", Music.class);
-        MusicPlayer player = new MusicPlayer(music);
-        player.playMusic();
+//
+//        Music music = context.getBean("classicalMusic", Music.class);
+//        MusicPlayer player = new MusicPlayer(ClassicalMusic);
+//        player.playMusic();
+//MusicPlayer player=context.getBean("musicPlayer",MusicPlayer.class);
+//player.playMusic();
+        Computer computer=context.getBean("computer",Computer.class);
+        System.out.println(computer);
         context.close();
     }
 
